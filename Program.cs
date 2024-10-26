ab6bff5ac1ac47efcd5364f920ac1511ed037536
abdoul <300139695@monboreal.ca>
2024-10-25 23:44:31 -0400

    ajout de l;instance utilisateur et les objets associer au paiement

--- Program.cs
+++ Program.cs
@@ -20,6 +20,24 @@ internal class Program
 
         b1.afficheDetail();
         b2.afficheDetail();
+
+
+
+        
+        Utilisateur utilisateur = new Utilisateur("abdoul aziz sow");
+
       
+        CarteCredit achat1 = new CarteCredit(200.0, "Achat d'ordinateur portable", 4321);
+        CarteCredit achat2 = new CarteCredit(30.0, "Achat de café", 8765);
+        CarteCredit achat3 = new CarteCredit(120.0, "Achat de vêtements", 6543);
+
+        utilisateur.AjouterPaiement(achat1);
+        utilisateur.AjouterPaiement(achat2);
+        utilisateur.AjouterPaiement(achat3);
+
+      
+        utilisateur.afficheInfos();
+
+
     }
 }
