b205b7de0150ab55135b06849047803894b521f7
abdoul <300139695@monboreal.ca>
2024-10-25 23:17:14 -0400

    ajout de la classe PayPal et des instances

---
+++ PayPal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class PayPal:Paiement
+    {
+            public string Courriel { get; set; }
+
+            public PayPal(double montant, string description, string email) : base(montant, description)
+            {
+                Courriel = email;
+            }
+
+            public override void afficheDetail()
+            {
+                Console.WriteLine($"le montant est : {Montant}, la description : {Description},l'email est :{Courriel}");
+            }
+
+   
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,11 +5,21 @@ internal class Program
     public static void Main(string[] args)
     {
         CarteCredit a1 = new CarteCredit(100.0, "Achat de ballons", 1234);
-        CarteCredit a2 = new CarteCredit(100.0, "Achat de livres", 1234);
-        CarteCredit a3 = new CarteCredit(100.0, "Achat de vetemens", 1234);
+        CarteCredit a2 = new CarteCredit(200.0, "Achat de livres", 1234);
+        CarteCredit a3 = new CarteCredit(300.0, "Achat de vetemens", 1234);
 
         a1.afficheDetail();
         a2.afficheDetail();
         a3.afficheDetail();
+
+
+
+        PayPal b1 = new PayPal(100.0, "Achat de ballons","abdoul207@gmail.com");
+        PayPal b2 = new PayPal(200.0, "Achat de livres", "Azizisow207@gmail.com");
+       
+
+        b1.afficheDetail();
+        b2.afficheDetail();
+      
     }
 }
