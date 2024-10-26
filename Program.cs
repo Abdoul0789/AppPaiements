a084a0e24179ae025cf87118be0962b1433ae269
abdoul <300139695@monboreal.ca>
2024-10-25 22:59:55 -0400

    ajout de la classe  CarteCredit et des instances

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
-﻿internal class Program
+﻿using AppPaiements;
+
+internal class Program
 {
-    private static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        CarteCredit a1 = new CarteCredit(100.0, "Achat de ballons", 1234);
+        CarteCredit a2 = new CarteCredit(100.0, "Achat de livres", 1234);
+        CarteCredit a3 = new CarteCredit(100.0, "Achat de vetemens", 1234);
+
+        a1.afficheDetail();
+        a2.afficheDetail();
+        a3.afficheDetail();
     }
 }
